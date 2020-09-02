5ec93cac8e1f616077681b1428d437de3a0db602
Vinicius <Shingueko_@hotmail.com>
2020-09-01 23:21:10 -0300

    feat: adiciona exercício 26

---
+++ exercicio26.py
@@ -0,0 +1,6 @@
+#Conversao de m² para hectares
+#Formula H = M * 0.0001
+M = float(input("Digite o valor da área em metros quadrados: "))
+H = M * 0.0001
+
+print("O valor da área em hectares é: ", H)
