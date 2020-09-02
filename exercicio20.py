e7186229a1a9c9bc31e60f245d1913b9df9c6ffc
Vinicius <Shingueko_@hotmail.com>
2020-09-01 22:52:02 -0300

    feat: adiciona exercício 20

---
+++ exercicio20.py
@@ -0,0 +1,6 @@
+#Atividade de conversao de quilogramas para libras
+#Formula L = K/0.45
+K = float(input("Digite o valor da massa que quilogramas: "))
+L = K/0.45
+
+print("O valor da massa em libras é: ", '%.2f' % L)
