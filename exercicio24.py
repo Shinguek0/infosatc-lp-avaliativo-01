5f2a2857365b9b66dd039451be7358bc118ce3e0
Vinicius <Shingueko_@hotmail.com>
2020-09-01 23:20:54 -0300

    feat: adiciona exercício 24

---
+++ exercicio24.py
@@ -0,0 +1,6 @@
+#Conversao de m² para acres
+#Formula A = M * 0.000247
+M = float(input("Digite o valor da área em metros quadrados: "))
+A = M * 0.000247
+
+print("O valor da área em Acres é: ", A) 
