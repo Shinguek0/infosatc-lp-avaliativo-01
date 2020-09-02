19d0119cf3994265c515ae061d0a49ecaac6d642
Vinicius <Shingueko_@hotmail.com>
2020-09-01 22:45:54 -0300

    feat: adiciona exercício 18

---
+++ exercicio18.py
@@ -0,0 +1,6 @@
+#Conversao de metros cubicos para litros
+#Formula Litros = 1000 * m³
+
+m3 = float(input("Digite uma valor em metros cubicos: "))
+L  = 1000 * m3;
+print("O valor convertido em litros: ", '%.f' % L,"L")
