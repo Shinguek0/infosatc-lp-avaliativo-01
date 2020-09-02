3ba1a30e50869d73fc3e1b7cfa18f7b7b7a2a1a1
Vinicius <Shingueko_@hotmail.com>
2020-09-01 22:33:53 -0300

    feat: adiciona exercício 13

---
+++ exercicio13.py
@@ -0,0 +1,6 @@
+#Atividade de conversao de uma distancia em quilimetros para milhas
+#Formula M = K/1,61
+K = float(input("Digite a distancia em quilometros: "))
+M = K/1.61
+
+print("A distancia em milhas é: ", '%.3f' % M)
