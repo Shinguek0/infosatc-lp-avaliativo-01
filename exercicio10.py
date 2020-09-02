ed8f8ec6467eb0a23653d77c09840af2374013cb
Vinicius <Shingueko_@hotmail.com>
2020-09-01 22:22:14 -0300

    feat: adiciona exercício 10

---
+++ exercicio10.py
@@ -0,0 +1,7 @@
+#Conversao de Km/h em m/s
+#Formula M = K/3.6
+
+K = float(input("Digite a velocidade em quilometros por hora: "))
+M = K/3.6
+
+print("A velocidade em Metros por segundo é: ", M, "m/s")
